f38a2c2c6360c5d87dcea87183c5d798d308232e
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-12-07 20:36:53 +0700

    fix bug register

--- src/models/registerPekerja.js
+++ src/models/registerPekerja.js
@@ -1,18 +1,15 @@
 const Pool = require('../config/db');
 
 const createUserPekerja = async (data) => {
-  const { nama, email, passwordHashed, phone, photo, job_desk, domisili, tempat_kerja, deskripsi_singkat, id_user } = data;
+  const { nama, email, passwordHashed, phone } = data;
   return new Promise((resolve, reject) => {
-    Pool.query(
-      `INSERT INTO users_pekerja (id_user, nama, email, phone, password, photo, job_desk, domisili, tempat_kerja, deskripsi_singkat) VALUES ('${id_user}', '${nama}', '${email}', '${phone}', '${passwordHashed}', '${photo}', '${job_desk}', '${domisili}', '${tempat_kerja}', '${deskripsi_singkat}')`,
-      (err, result) => {
-        if (!err) {
-          return resolve(result);
-        } else {
-          return reject(err);
-        }
+    Pool.query(`INSERT INTO users_pekerja (id_user, nama, email, phone, password, photo, job_desk, domisili, tempat_kerja, deskripsi_singkat) VALUES ('${id_user}', '${nama}', '${email}', '${phone}', '${passwordHashed}')`, (err, result) => {
+      if (!err) {
+        return resolve(result);
+      } else {
+        return reject(err);
       }
-    );
+    });
   });
 };
 
--- src/models/registerPerekrut.js
+++ src/models/registerPerekrut.js
@@ -1,10 +1,10 @@
 const Pool = require('../config/db');
 
 const createUserPerekrut = async (data) => {
-  const { nama, email, nama_perusahaan, jabatan, phone, passwordHashed, bidang, provinsi, kota, deskripsi_singkat, email_perusahaan, linked_in, id_user } = data;
+  const { nama, email, nama_perusahaan, jabatan, phone, passwordHashed, id_user } = data;
   return new Promise((resolve, reject) => {
     Pool.query(
-      `INSERT INTO users_perekrut (id_user, nama, email, nama_perusahaan, jabatan, phone, password, bidang, provinsi, kota, deskripsi_singkat, email_perusahaan, linked_in) VALUES ('${id_user}', '${nama}', '${email}', '${nama_perusahaan}', '${jabatan}', '${phone}', '${passwordHashed}', '${bidang}', '${provinsi}', '${kota}', '${deskripsi_singkat}', '${email_perusahaan}', '${linked_in}')`,
+      `INSERT INTO users_perekrut (id_user, nama, email, nama_perusahaan, jabatan, phone, password, bidang, provinsi, kota, deskripsi_singkat, email_perusahaan, linked_in) VALUES ('${id_user}', '${nama}', '${email}', '${nama_perusahaan}', '${jabatan}', '${phone}', '${passwordHashed}')`,
       (err, result) => {
         if (!err) {
           return resolve(result);
